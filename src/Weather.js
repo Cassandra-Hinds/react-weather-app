34ff32092a6781c28616d97fe6eb108e159d68b0
Cassandra-Hinds <Cassandrahinds9@gmail.com>
2022-02-05 17:37:49 -0500

    Transferred my code from CodeSandbox to VS code

---
+++ src/Weather.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    date: "Friday 12:30pm",
+    weather: "Cloudy",
+    weatherIcon: "https://openweathermap.org/img/wn/10d@2x.png",
+    humidity: 70,
+    wind: 70,
+  };
+  return (
+    <div>
+      <form>
+        <input
+          type="search"
+          placeholder="Search | Enter a city"
+          className="
+form-control
+shadow-none
+p-3
+mb-5
+bg-light
+rounded
+border border-primary border border-2
+"
+        />
+      </form>
+      <button>Current Location</button>
+      <div>
+        <div className="city">{weatherData.city}</div>
+        <div className="day-and-time">{weatherData.date}</div>
+        <div className="weather">{weatherData.weather}</div>
+        <div className="clearfix current-temperature">
+          <img
+            src={weatherData.weatherIcon}
+            alt="weather icon"
+            className="float-left"
+          />
+          <span className="float-left">
+            <span>70</span>
+            <span className="fahren">°F</span>
+          </span>
+        </div>
+      </div>
+      <div className="humidity">
+        <i className="fas fa-temperature-high humidity-icon"></i> Humidity:
+        <span> {weatherData.humidity}</span>%
+      </div>
+      <div className="wind">
+        <i className="fas fa-wind wind-icon"></i>Wind:
+        <span> {weatherData.wind}</span>m/h
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Weather from "./Weather";
+import "./App.css";
+
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  return (
+    <div class="container">
+      <Weather />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
